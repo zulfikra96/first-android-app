e43fcd3b7111c0764280ff03df95f3e74a07e03f
zulfikra96 <zulfikralahmudin@gmail.com>
2017-02-25 08:38:56 +0700

    created auth component

---
+++ app/components/StatusbarBackground.js
@@ -0,0 +1,26 @@
+import React,{Component} from 'react';
+import {
+    View,
+    StyleSheet
+} from 'react-native';
+
+export default class StatusbarBackground extends Component{
+    
+    render(){
+        return(
+           <View style={styles.statusbarBackground}>
+                
+           </View>
+        );
+    }
+
+
+}
+
+const styles =  StyleSheet.create({
+
+    statusbarBackground:{
+        height:20,
+    }
+
+});
